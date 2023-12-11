6bea1f059b703617b3bae4ce315b77a4f5e6cb3b
Pavelosky <pawel.bialkowskigru@gmail.com>
2023-12-11 20:18:08 +0100

    draw ther table

--- Snooker/sketch.js
+++ Snooker/sketch.js
@@ -1,8 +1,20 @@
+let ball;
+let balls = [];
+let ballWidth;
+let pocketSize;
+let table;
+
 function setup() {
-    createCanvas(900, 600);
-    background(0);
+    createCanvas(920, 470);
+    background(200);
+    fill(44,130,87);
+    rectMode(CENTER);
+
+    ballWidth = 25;
+    pocketSize = ballWidth*1.5;
+
 }
 
 function draw() {
-
+    table = rect(width/2, height/2, width -20, height-20);
 }
