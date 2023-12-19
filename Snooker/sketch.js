095f09fb955190670a7fc2326638d078bec2253f
Pavelosky <pawel.bialkowskigru@gmail.com>
2023-12-19 20:56:53 +0100

    added cue ball and cue

--- Snooker/sketch.js
+++ Snooker/sketch.js
@@ -8,24 +8,30 @@ Constraint = Matter.Constraint;
 
 let table;
 // to change the size of the table change only tableHeight, rest is scaled to it.
-let tableHeight = 600;
+let tableHeight = 400;
 let tableWidth = tableHeight*2;
 
 let ball;
 let balls = [];
+let cueBall;
+let cueBallRadius;
 let ballWidth;
 let pocketSize;
-
-
+let cue;
+let cueLength;
 
 function setup() {
     createCanvas(tableWidth,tableHeight);
     engine = Engine.create();
+    engine.gravity.y = 0;
     
     rectMode(CENTER);
 
-    ballWidth = tableWidth/36
+    ballWidth = tableWidth/36;
     pocketSize = ballWidth*1.5;
+    cueBallRadius = ballWidth / 2;
+    cueLength = 100;
+
     let verticesCushion = [
         { x: (tableWidth / 4) + (tableWidth/120), y: tableHeight+(tableHeight/15)},
         { x: (tableWidth *3/4) -(tableWidth/120), y: tableHeight+(tableHeight/15)},
@@ -33,16 +39,19 @@ function setup() {
         { x: tableWidth / 4 + (tableWidth/24), y: tableHeight},
       ];
 
-    ball = Matter.Bodies.circle(tableWidth/2,tableHeight/2, ballWidth/2, {restituton:0.5, friction: 0.01});
+    cueBall = Matter.Bodies.circle(tableWidth / 2, tableHeight / 2, cueBallRadius, { restitution: 0.5, friction: 0.01 });
+    ball = Matter.Bodies.circle(tableWidth/2,tableHeight/2, ballWidth/2, {restitution:0.5, friction: 0.01});
+    cue = Matter.Bodies.rectangle(cueBall.position.x - cueLength / 2 - 20, cueBall.position.y, cueLength, 5, { isStatic: true });
+
     cushion = Matter.Bodies.fromVertices(tableWidth * 1/4, tableHeight, verticesCushion, { isStatic: true });
     cushion2 = Matter.Bodies.fromVertices(tableWidth * 3/4, tableHeight, verticesCushion, { isStatic: true });
     cushion3 = Matter.Bodies.fromVertices(tableWidth * 1/4, 0, verticesCushion, { isStatic: true, angle: PI});
     cushion4 = Matter.Bodies.fromVertices(tableWidth * 3/4, 0, verticesCushion, { isStatic: true, angle: PI});
     cushion5 = Matter.Bodies.fromVertices(tableWidth, tableHeight/2, verticesCushion, { isStatic: true, angle: PI*1.5});
     cushion6 = Matter.Bodies.fromVertices(0, tableHeight/2, verticesCushion, { isStatic: true, angle: PI*0.5});
-    Composite.add(engine.world, [cushion, cushion2, cushion3, cushion4, cushion5, cushion6, ball]);
+    Composite.add(engine.world, [cushion, cushion2, cushion3, cushion4, cushion5, cushion6, ball, cueBall]);
     
-    for (let i = 0; i < 7; i++) {
+    for(let i = 0; i < 7; i++){
         generateBalls()
     }
 
@@ -58,6 +67,16 @@ function draw() {
     line(tableWidth/5, 0, tableWidth/5, tableHeight)
     arc(tableWidth/5, tableHeight/2, tableHeight/3, tableHeight/3, PI*0.5, PI*1.5, CHORD)
     
+
+    // Update cue ball position based on mouse
+    Body.setPosition(cueBall, { x: mouseX, y: mouseY });
+
+    // draw white ball
+    fill(255);
+    stroke(50);
+    drawVertices(cueBall.vertices);
+
+    // draw cushions
     noStroke()
     fill(50)
     drawVertices(cushion.vertices)
@@ -67,14 +86,19 @@ function draw() {
     drawVertices(cushion5.vertices)
     drawVertices(cushion6.vertices)
 
-    fill(200, 50, 50)
-    stroke(50)
-    drawVertices(ball.vertices);
-
-
+    // draw balls
     for (let i = 0; i < balls.length; i++) {
+        fill(200, 50, 50)
+        stroke(1)
         drawVertices(balls[i].vertices)
     }
+
+    // Draw cue
+    fill(139, 69, 19); 
+    drawVertices(cue.vertices);
+    Body.setPosition(cue, { x: cueBall.position.x - cueLength, y: cueBall.position.y });
+
+
 }
 
 function generateBalls(x,y){
